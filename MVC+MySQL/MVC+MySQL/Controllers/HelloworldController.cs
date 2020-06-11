91ce112a425d929ea7965c626ddf41b3949b4964
BeerHub <beervip1997@gmail.com>
2020-06-11 17:27:09 +0700

    up

---
+++ MVC+MySQL/MVC+MySQL/Controllers/HelloworldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVC_MySQL.Controllers
+{
+    public class HelloworldController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Wellcome(string name, int age)
+        {
+            ViewData["name"] = "Hello " + name;
+            ViewData["age"] = "อายุ " + age;
+
+            return View();
+        }
+    }
+}
---
+++ MVC+MySQL/MVC+MySQL/data/MvcMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_MySQL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_MySQL.data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
